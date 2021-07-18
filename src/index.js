82819314e3859bb27dc629ca58b278a3cffbc1f8
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-07-18 13:07:53 +0300

    init: finish task typical arrays problems
    Finish tasl typical arrays problems. All tests pasted.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if (array == null || array.length == 0){
+        return 0;
+    }
+
+    let resultMin = 0;
+    for (let i = 0; i < array.length; i++){
+        if (resultMin > array[i]){
+            resultMin = array[i]; 
+        }
+    }
+  return resultMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (array == null || array.length == 0){
+    return 0;
+  }
+
+  let resultMax = 0;
+  for (let i = 0; i < array.length; i++){
+    if (resultMax < array[i]){
+        resultMax = array[i];
+    }
+  }
+
+  return resultMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ 
+  if (array == null || array.length == 0){
+    return 0;
+  }
+  
+  let resultAvg = 0;
+  let avgArr = 0;
+    for (let i = 0; i < array.length; i++){
+    avgArr += array[i];
+    }
+  resultAvg = avgArr / array.length;
+  return resultAvg;
 }
